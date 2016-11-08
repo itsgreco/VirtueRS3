0864ad36557071c41b42f579da3bee6f5cbc2508
jarryd229 <jarrydbrighton@hotmail.com>
2016-11-09 10:06:31 +1100

    woodcutting.js
    woodcutting.js

--- repository/scripts/skill/Woodcutting.js
+++ repository/scripts/skill/Woodcutting.js
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2014 Virtue Studios
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @author Im Frizzy <skype:kfriz1998>
+ * @author Kayla <skype:ashbysmith1996>
+ * @author Frosty Teh Snowman <skype:travis.mccorkle>
+ * @author Arthur <skype:arthur.behesnilian>
+ * @author Sundays211
+ * @since 05/11/2014
+ */
+
 var TreeType = {
     NORMAL : {
         level : 1,
@@ -223,7 +254,7 @@ var Hatchet = {
 	CRYSTAL : {
 		level : 71,
 		time : 16,
-		anim : 25003, //25182 for canoe ?
+		anim : 25169, //25182 for canoe ?
 		itemID : 32645
 	}
 };
@@ -387,14 +418,16 @@ var Woodcutting = {
 				return;
 			}
 			api.runAnimation(player, hatchet.anim);
-			api.sendMessage(player, "You swing your hatchet at the tree.", MesType.GAME_SPAM);
+			api.sendMessage(player, "You swing your hatchet at the tree.");
 			var delay = this.getDelay(player, tree.type, hatchet);//Calculates the time taken to mine this rock
+			var baseDelay = this.getDelay(player, tree.type, hatchet);//Calculates the time taken to mine this rock
 			var Action = Java.extend(Java.type('org.virtue.game.entity.player.event.PlayerActionHandler'), {	
 				process : function (player) {
 					if (location.getId() != tree.tree) {
 						return false;//This means the tree has already been felled
 					}
 					api.runAnimation(player, hatchet.anim);
+					api.sendMessage(player, "Timer delay" + delay);
 					if (delay <= 0) {
 						api.addExperience(player, Stat.WOODCUTTING, tree.type.xp, true);
 						api.addCarriedItem(player, tree.type.logId, 1);
@@ -406,14 +439,16 @@ var Woodcutting = {
 							api.sendMessage(player, "Not enough space in your inventory.");
 							return true;
 						} else {
+							delay = baseDelay;
 							return false;
-						}						
+						}	
 					}
 					delay--;
 					return false;
 				},
 				stop : function (player) {
 					api.stopAnimation(player);
+					delay = baseDelay;
 				}
 				
 			});
